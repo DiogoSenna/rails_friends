766b1e8370d23828f3dff6ec7c4ca13f9f9c361d
Diogo Senna <disenna@gmail.com>
2021-02-10 19:17:47 -0300

    fixed bootstrap alerts

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,9 @@
-import { environment } from '@rails/webpacker'
-
-import { ProvidePlugin } from 'webpack'
-environment.plugins.append('Provide', 
-    new ProvidePlugin({
-        s: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
-
-export default environment
+const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  }))
+module.exports = environment
